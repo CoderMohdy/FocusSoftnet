713616210aeb1e80634251a392b49e3d0fb79bfe
CoderMohdy <mohammedmohiuddin.siddiqui@gmail.com>
2026-02-24 15:13:37 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+OPENAI_API_KEY = "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"
+GOOGLE_MAPS_KEY = "OPTIONAL_FOR_NOW"///
+FOCUS_SOFTNET_NAME = "Focus Softnet"
+SALES_EMAIL = "mohammedmohiuddin.siddiqui@gmail.com"
